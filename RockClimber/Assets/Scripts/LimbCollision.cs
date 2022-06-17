37e4aa00e303f7321ced07e45afc810a0cea18da
Killyose <56149070+Beauxbatons@users.noreply.github.com>
2022-06-17 23:26:45 +0300

    character jump

---
+++ RockClimber/Assets/Scripts/LimbCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbCollision : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+
+    void Start()
+    {
+        playerMovement = GameObject.FindObjectOfType<PlayerMovement>().GetComponent<PlayerMovement>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        playerMovement.isGrounded = true;
+    }
+}
---
+++ RockClimber/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce;
+
+    public Rigidbody hips;
+    public bool isGrounded;
+
+    void Start()
+    {
+        hips = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetAxis("Jump") > 0)
+        {
+            if(isGrounded)
+            {
+                hips.AddForce(new Vector3(0, jumpForce, 0));
+                isGrounded = false;
+            }
+        }
+    }
+}
