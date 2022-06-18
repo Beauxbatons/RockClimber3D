d7d2f5375aabf74c732acb9b73799f86ff53a13b
Beauxbatons <exaltedgames@hotmail.com>
2022-06-19 02:48:27 +0300

    new character physics

---
+++ RockClimber/Assets/Scripts/Snaping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snaping : MonoBehaviour
+{
+    public GameObject a;
+    public GameObject b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.transform.position = b.transform.position;
+
+    }
+}
