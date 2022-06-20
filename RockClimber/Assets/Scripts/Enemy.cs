b4ac218d80cbd1c18c81bbbc750eb93bee34927d
Killyose <56149070+Beauxbatons@users.noreply.github.com>
2022-06-20 23:14:16 +0300

    edits

--- RockClimber/Assets/Scripts/Enemy.cs
+++ RockClimber/Assets/Scripts/Enemy.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float dirx, dirY;
+    Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dir
     }
 }
