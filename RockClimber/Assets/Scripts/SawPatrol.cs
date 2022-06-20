109d11ce115e88026e39e84013305f542117fd3c
Beauxbatons <exaltedgames@hotmail.com>
2022-06-20 14:26:10 +0300

    Menu

---
+++ RockClimber/Assets/Scripts/SawPatrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawPatrol : MonoBehaviour
+{
+    public Transform[] wayPoints;
+    public int speed;
+
+    private int wayPointIndex;
+    private float dista;
+
+    void Start()
+    {
+        wayPointIndex = 0;
+        transform.LookAt(wayPoints[wayPointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dista = Vector3.Distance(transform.position, wayPoints[wayPointIndex].position);
+        if(dista < 1f)
+        {
+            IncreaseIndex();
+        }
+        Patrol();
+    }
+
+
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        wayPointIndex++;
+        if(wayPointIndex >= wayPoints.Length)
+        {
+            wayPointIndex = 0;
+        }
+
+        transform.LookAt(wayPoints[wayPointIndex].position);
+    }
+}
